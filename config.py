0b89e650bc0f3d416d1530f476b0ac254c785081
John-Cristopher <john.cristopher.senai@gmail.com>
2025-12-11 15:55:43 -0300

    fix

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ USUARIO_ADMIN = "Amazon_platt"
 SENHA_ADMIN = "Umamusume123"
 
 # Variável de controle de ambiente, poderá ser "local" ou "produção"
-ambiente = "local"
+ambiente = "produção"
 
 if ambiente == "local":
     # ------ INFORMAÇÕES DO SEU BLOG LOCAL, DEIXE COMO ESTÁ
@@ -13,7 +13,7 @@ if ambiente == "local":
     DATABASE = "blog_john"
 elif ambiente == "produção":
     # ------ INFORMAÇÕES VINDAS DO DATABASE DO PYTHON ANYWHERE
-    HOST = "link python anywhere"
-    USER = "user python anywhere"
-    PASSWORD = "senha database python anywhere"
-    DATABASE = "nome do database python anywhere"
+    HOST = "JohnCristopher.mysql.pythonanywhere-services.com"
+    USER = "JohnCristopher"
+    PASSWORD = "GYMkhanaSuBaru8r477"
+    DATABASE = "JohnCristopher$MidnightBlog"
