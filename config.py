4cacb65418297f3b65abb8ad6596280acec11db4
John-Cristopher <john.cristopher.senai@gmail.com>
2025-12-11 13:13:08 -0300

    fix

---
+++ config.py
@@ -0,0 +1,19 @@
+SECRET_KEY = "Chicken"
+USUARIO_ADMIN = "Amazon_platt"
+SENHA_ADMIN = "Umamusume123"
+
+# Variável de controle de ambiente, poderá ser "local" ou "produção"
+ambiente = "local"
+
+if ambiente == "produção":
+    # ------ INFORMAÇÕES VINDAS DO DATABASE DO PYTHON ANYWHERE
+    HOST = "link python anywhere"
+    USER = "user python anywhere"
+    PASSWORD = "senha database python anywhere"
+    DATABASE = "nome do database python anywhere"
+else:
+    # ------ INFORMAÇÕES DO SEU BLOG LOCAL, DEIXE COMO ESTÁ
+    HOST = "localhost"
+    USER = "root"
+    PASSWORD = "senai"
+    DATABASE = "blog_john"
